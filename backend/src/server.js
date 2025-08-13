e644814f61c1979094aeacf6f24c37af3c8f9cf3
H.M.K.K.Premarathna <kavindukavishkacampus@gmail.com>
2025-08-13 09:44:22 +0530

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,43 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import rateLimiter from "./middleware/ratelimiter.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-dotenv.config({ path: "./src/.env" });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-//middleware to parse JSON
-app.use(cors({ origin: "http://localhost:5173", }));
-app.use(express.json());
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
+
 
 
 app.use("/api/notes", notesRoutes);
 
-// Once the database connected then only the application should start
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Server started in port: ", PORT);
+    console.log("Server started on PORT:", PORT);
   });
 });
